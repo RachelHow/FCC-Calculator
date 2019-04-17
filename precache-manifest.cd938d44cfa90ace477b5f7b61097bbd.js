9971835c20fd874bb5fa873f40e568364e748e14
rachelhow <rachelhow5242@gmail.com>
2019-04-17 23:35:33 +0800

    Updates

---
+++ precache-manifest.cd938d44cfa90ace477b5f7b61097bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "780499e3293c00e85e41",
+    "url": "/fcc-c/static/js/runtime~main.8154b048.js"
+  },
+  {
+    "revision": "e36bd4b03c2d3acff602",
+    "url": "/fcc-c/static/js/main.4e0a2bde.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/fcc-c/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e36bd4b03c2d3acff602",
+    "url": "/fcc-c/static/css/main.6407f2c1.chunk.css"
+  },
+  {
+    "revision": "418c4f4f4e4255c061581fa175f5fbd5",
+    "url": "/fcc-c/index.html"
+  }
+];
